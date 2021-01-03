0e33ca26172d5bd7bf4b0989f70fc578dbcd9bfe
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 17:30:09 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -17,6 +17,18 @@ export default function Layout({ children, home }) {
           <meta name="description" content="I'm a regular person who does beatmaking and sound engineering. I used to be into indie game development and was on many development teams." />
           <meta name="og:title" content={siteTitle} />
           <meta name="og:image" content="https://greysoul.ru/images/seo.jpg" />
+<script type="text/javascript">
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(70946869, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/70946869" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         </Head>
         <header className={styles.header}>
           <Banner home={home} name={name}/>
