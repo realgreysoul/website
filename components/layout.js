4355cf0af4be905448768d14fcafaa11231c7c2e
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 12:54:18 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import Footer from './footer'
 import Navbar from './navbar'
 import Banner from './banner'
 
-const name = 'Michael Westerby'
+const name = 'Greysoul'
 export const siteTitle = name
 
 export default function Layout({ children, home }) {
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
       <div className={styles.container}>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Michael Westerby - Personal Website" />
+          <meta name="description" content="I'm a regular person who does beatmaking and sound engineering. I used to be into indie game development and was on many development teams." />
           <meta name="og:title" content={siteTitle} />
         </Head>
         <header className={styles.header}>
