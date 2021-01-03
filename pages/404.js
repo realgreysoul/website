f4a3aa163d29558e8e1c0e11b67eac48359e2b9f
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 15:02:25 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -3,6 +3,8 @@ import Head from 'next/head'
 
 export default function Custom404() {
   return (
+    <div>
+    <Layout>
       <Head>
         <title>404: Page Not Found — Greysoul</title>
         <meta name="og:title" content="404: Page Not Found — Greysoul" />
@@ -10,5 +12,7 @@ export default function Custom404() {
       <h1 className={utilStyles.heading2Xl}>404</h1>
       <h2 className={utilStyles.headingMd}>Page Not Found</h2>
       <p className={utilStyles.headingMd}>If you reached this page by mistake, return to the <Link href={`/`}>home page</Link></p>
+    </div>
+    </Layout>
   )
 }
