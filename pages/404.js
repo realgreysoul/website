4a863f767ca954ca3a3cbad2f2f12a139c26f5d7
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 14:55:32 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,16 @@
 import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head'
 
 export default function Custom404() {
   return (
+      <Head>
+        <title>404: Page Not Found — Greysoul</title>
+        <meta name="og:title" content="404: Page Not Found — Greysoul" />
+      </Head>
     <div>
-      <p className={utilStyles.heading2Xl}>404 Page Not Found</p>
+      <h1 className={utilStyles.heading2Xl}>404</h1>
+      <h2 className={utilStyles.headingMd}>Page Not Found</h2>
+      <p className={utilStyles.headingMd}>If you reached this page by mistake, return to the <Link href={`/`}>home page</Link></p>
     </div>
   )
 }
