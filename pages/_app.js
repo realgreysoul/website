dca2a2458d93be17b14026ec23d83320bda4c4dc
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 16:02:46 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,5 @@ import '../styles/global.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+import Router from "next/router";
+import withYM from "next-ym";
