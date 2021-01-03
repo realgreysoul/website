f68e9ce42551bf1f682d847a0ec03cc71b3c2aa0
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 14:08:26 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -6,6 +6,7 @@ import Banner from './banner'
 
 const name = 'Greysoul'
 export const siteTitle = name
+export const OGImage = /images/seo.jpg
 
 export default function Layout({ children, home }) {
   return (
@@ -16,6 +17,7 @@ export default function Layout({ children, home }) {
           <link rel="icon" href="/favicon.ico" />
           <meta name="description" content="I'm a regular person who does beatmaking and sound engineering. I used to be into indie game development and was on many development teams." />
           <meta name="og:title" content={siteTitle} />
+          <meta name="og:image" content={OGImage} />
         </Head>
         <header className={styles.header}>
           <Banner home={home} name={name}/>
