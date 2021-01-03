6d43572dc4aeaf189a367302bd46e49b10c6788e
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-03 15:03:25 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -12,7 +12,7 @@ export default function Custom404() {
       <h1 className={utilStyles.heading2Xl}>404</h1>
       <h2 className={utilStyles.headingMd}>Page Not Found</h2>
       <p className={utilStyles.headingMd}>If you reached this page by mistake, return to the <Link href={`/`}>home page</Link></p>
-    </div>
     </Layout>
+    </div>
   )
 }
