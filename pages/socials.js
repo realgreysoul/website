b9af74d8db8eb3674c438b976264456b5f7a3209
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 15:27:26 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -1,33 +1,18 @@
+import Layout, {siteTitle} from '../components/layout'
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
-import Footer from '../components/footer'
-import styles from '../styles/footer.module.css'
+import { getSortedProjectsData } from '../lib/projects'
+import Footer from './footer'
 
-export default function Footer() {
-  const vk = 'realgreysoul';
-  const instagram = 'realgreysoul';
-  const twitch = 'realgreysoul';
-  const blog = 'mislisolya';
-  
-  const spacer = ", "
-  const spacer2 = " & "
-
+export default function Projects({ allProjectsData }) {
   return (
     <Layout>
       <Head>
-        <title>Projects — {siteTitle}</title>
-          <meta name="og:title" content="Projects — Greysoul" />
+        <title>Socials — {siteTitle}</title>
+          <meta name="og:title" content="Socials — Greysoul" />
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <footer className={styles.footer}>
-        <h1 class="layout_header__1OJ41">Socials</h1>
-        <a href={`https://vk.com/${vk}`} target="_blank">VK</a>{spacer}
-        <a href={`https://www.instagram.com/${instagram}/`} target="_blank">Instagram</a>{spacer}
-        <a href={`https://www.twitch.tv/${twitch}`} target="_blank">Twitch</a>{spacer2}
-        <a href={`https://t.me/${blog}`} target="_blank">Blog</a>
-      </footer>
+        <Footer />
       </section>
     </Layout>
   )
