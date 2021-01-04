909657743efbec519348c248527a906fb96c9b75
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 17:32:04 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,6 +5,7 @@ export default function Footer() {
       <footer className={styles.footer}>
         <small class="utils_lightText__12Ckm">
         <p>© 2021 Greysoul</p>
+        <p>Based on the Michael Westerby <Link href='https://github.com/mwesterby/michaelwesterby.com'><a>repository</a></Link></p>
         </small>
       </footer>
   )
