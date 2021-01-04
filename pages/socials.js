5f7623f9664ad1b483a091bb5e54f71d2bf270e6
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 23:16:01 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -14,14 +14,19 @@ export default function Projects({ allProjectsData }) {
         <h1 className={utilStyles.headingXL}>Socials</h1>
         <a href={`https://vk.com/realgreysoul`} target="_blank"><strong>VK</strong></a>
         <br>
+        </br>
         <a href={`https://www.instagram.com/realgreysoul/`} target="_blank"><strong>Instagram</strong></a>
         <br>
+        </br>
         <a href={`https://t.me/greysoul`} target="_blank"><strong>Telegram</strong></a>
         <br>
+        </br>
         <a href={`https://t.me/mislisolya`} target="_blank"><strong>Blog</strong></a>
         <br>
+        </br>
         <a href={`https://steamcommunity.com/id/realgreysoul/`} target="_blank"><strong>Steam</strong></a>
         <br>
+        </br>
         <a href={`https://socialclub.rockstargames.com/member/RealGreysoul/`} target="_blank"><strong>Social Club</strong></a>
       </section>
     </Layout>
