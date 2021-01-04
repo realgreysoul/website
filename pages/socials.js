67266e2c20ab3bfe794d4809d137c40395817fb3
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 15:19:38 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
-import Footer from './footer'
+import Footer from '../components/footer'
 
 export default function Home({ allPostsData }) {
   return (
