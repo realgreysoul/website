4c0ec0288c06ab4ff29cc7c38c45daf984cd79ea
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 15:18:12 +0700

    Create socials.js

---
+++ pages/socials.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Footer from './footer'
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <Footer />
+      </section>
+    </Layout>
+  )
+}
