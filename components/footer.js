51f34e88a0c915d55748d9ff69171701b3b78f3a
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 16:23:23 +0700

    Create footer.js

---
+++ components/footer.js
@@ -0,0 +1,10 @@
+import styles from '../styles/footer.module.css'
+
+  return (
+      <footer className={styles.footer}>
+        <small class="utils_lightText__12Ckm">
+        <p>© 2021 Greysoul</p>
+        </small>
+      </footer>
+  )
+}
