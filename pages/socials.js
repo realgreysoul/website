25ce837502a0993e5e4ecc1e7021001922e26d79
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 15:28:06 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -2,7 +2,7 @@ import Layout, {siteTitle} from '../components/layout'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedProjectsData } from '../lib/projects'
-import Footer from './footer'
+import Footer from '../components/footer'
 
 export default function Projects({ allProjectsData }) {
   return (
