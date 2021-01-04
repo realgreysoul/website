7e16935e23a2be4503be1d19342b938e635b0243
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 16:14:14 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -5,7 +5,6 @@ import utilStyles from '../styles/utils.module.css'
 export default function Projects({ allProjectsData }) {
   const vk = 'realgreysoul';
   const instagram = 'realgreysoul';
-  const twitch = 'realgreysoul';
   const blog = 'mislisolya';
   
   const spacer = ", "
@@ -20,8 +19,7 @@ export default function Projects({ allProjectsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h1 className={utilStyles.headingXL}>Socials</h1>
         <a href={`https://vk.com/${vk}`} target="_blank">VK</a>{spacer}
-        <a href={`https://www.instagram.com/${instagram}/`} target="_blank">Instagram</a>{spacer}
-        <a href={`https://www.twitch.tv/${twitch}`} target="_blank">Twitch</a>{spacer2}
+        <a href={`https://www.instagram.com/${instagram}/`} target="_blank">Instagram</a>{spacer2}
         <a href={`https://t.me/${blog}`} target="_blank">Blog</a>
       </section>
     </Layout>
