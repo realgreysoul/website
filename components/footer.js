f6dafb61988d702d3524cb7f97036f4ebb0c557d
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 12:03:19 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -6,13 +6,15 @@ export default function Footer() {
   const twitch = 'realgreysoul';
   const blog = 'mislisolya';
   
-  const spacer = " - "
+  const spacer = ", "
+  const spacer2 = " & "
 
   return (
       <footer className={styles.footer}>
+        <h1 class="layout_header__1OJ41">Socials</h1>
         <a href={`https://vk.com/${vk}`} target="_blank">VK</a>{spacer}
         <a href={`https://www.instagram.com/${instagram}/`} target="_blank">Instagram</a>{spacer}
-        <a href={`https://www.twitch.tv/${twitch}`} target="_blank">Twitch</a>{spacer}
+        <a href={`https://www.twitch.tv/${twitch}`} target="_blank">Twitch</a>{spacer2}
         <a href={`https://t.me/${blog}`} target="_blank">Blog</a>
       </footer>
   )
