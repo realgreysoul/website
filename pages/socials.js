6fc152cfd3621991550bdd64b56020bc90235c48
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-04 15:50:03 +0700

    Update socials.js

--- pages/socials.js
+++ pages/socials.js
@@ -1,7 +1,15 @@
 import Layout, {siteTitle} from '../components/layout'
 import Head from 'next/head'
 import utilStyles from '../styles/utils.module.css'
-import Footer from '../components/footer'
+
+export default function Footer() {
+  const vk = 'realgreysoul';
+  const instagram = 'realgreysoul';
+  const twitch = 'realgreysoul';
+  const blog = 'mislisolya';
+  
+  const spacer = ", "
+  const spacer2 = " & "
 
 export default function Projects({ allProjectsData }) {
   return (
@@ -11,8 +19,13 @@ export default function Projects({ allProjectsData }) {
           <meta name="og:title" content="Socials — Greysoul" />
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h1 className={utilStyles.headingXL}>Socials</h1>
-        <Footer />
+      <footer className={styles.footer}>
+        <h1 className={utilStyles.headingXL}>Socials</h1>
+        <a href={`https://vk.com/${vk}`} target="_blank">VK</a>{spacer}
+        <a href={`https://www.instagram.com/${instagram}/`} target="_blank">Instagram</a>{spacer}
+        <a href={`https://www.twitch.tv/${twitch}`} target="_blank">Twitch</a>{spacer2}
+        <a href={`https://t.me/${blog}`} target="_blank">Blog</a>
+      </footer>
       </section>
     </Layout>
   )
