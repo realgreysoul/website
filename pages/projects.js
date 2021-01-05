0a2ac688d86ee535a2bcb01be1f81abbbac1c510
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-05 14:42:22 +0700

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -44,48 +44,48 @@ export default function Projects({ allProjectsData }) {
         <div class="utils_listItemHeading__iji17">
         <a href="https://beats.greysoul.ru/" target="_blank">Greysoul Beats</a>
         </div>
+        <small class="utils_lightText__12Ckm"><p>Beats for all kinds of performers</p></small>
         <small>
         <ul class="utils_list__S7_pe">
         <li class="utils_listItem__2eJpJ">
         </li>
         </ul>
-        <small class="utils_lightText__12Ckm"><p>Beats for all kinds of performers</p></small>
         </small>
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
         <a href="https://vk.com/x1xbuch" target="_blank">X1xBuch</a>
         </div>
+        <small class="utils_lightText__12Ckm"><p>Music Distribution and Artist Management</p></small>
         <small>
         <ul class="utils_list__S7_pe">
         <li class="utils_listItem__2eJpJ">
         </li>
         </ul>
-        <small class="utils_lightText__12Ckm"><p>Music Distribution and Artist Management</p></small>
         </small>
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
         <a href="https://vk.com/angie_is_so_sexy" target="_blank">Angie</a>
         </div>
+        <small class="utils_lightText__12Ckm"><p>Music Distribution and Artist Management</p></small>
         <small>
         <ul class="utils_list__S7_pe">
         <li class="utils_listItem__2eJpJ">
         </li>
         </ul>
-        <small class="utils_lightText__12Ckm"><p>Music Distribution and Artist Management</p></small>
         </small>
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
         <a href="https://www.youtube.com/channel/UCfBYBmvY2JgKNA6prrfFqhw" target="_blank">грейсоль</a>
         </div>
+        <small class="utils_lightText__12Ckm"><p>A YouTube channel with content that even I don't know</p></small>
         <small>
         <ul class="utils_list__S7_pe">
         <li class="utils_listItem__2eJpJ">
         </li>
         </ul>
-        <small class="utils_lightText__12Ckm"><p>A YouTube channel with content that even I don't know</p></small>
         </small>
         </li>
         <li class="utils_listItem__2eJpJ">
