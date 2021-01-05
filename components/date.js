feedfc31a0fc285a397ccc87709fe4add274c27a
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-05 14:50:50 +0700

    Delete date.js

--- components/date.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns'
-
-export default function Date({ dateString, dateFormat }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, dateFormat ? dateFormat : 'do LLLL yyyy')}</time>
-}
