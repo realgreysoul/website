8b59436e671776316ec4b3dce9934dc20c375fe1
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-14 17:58:41 +0700

    Update banner.js

--- components/banner.js
+++ components/banner.js
@@ -1,27 +1,22 @@
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import styles from '../styles/banner.module.css'
-import Image from 'next/image'
 
 export default function Banner({home, name}) {
   const homeBanner = (<>
-    <Image
+    <img
       src="/images/profile.jpg"
-      width={162}
-      height={162}
       className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
       alt="Greysoul"
     />
     <h1 className={utilStyles.heading2Xl}>{name}</h1>
   </>)
-  
+
   const secondaryBanner = (<>
     <Link href="/">
       <a>
-        <Image
+        <img
           src="/images/profile.jpg"
-          width={108}
-          height={108}
           className={`${styles.headerImage} ${utilStyles.borderCircle}`}
           alt="Greysoul"
         />
