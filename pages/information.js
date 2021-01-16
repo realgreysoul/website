9ab7b53e021d62fef7c0865d713a645be8198dc1
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-16 17:45:48 +0700

    Update information.js

--- pages/information.js
+++ pages/information.js
@@ -116,7 +116,7 @@ export default function Projects({ allProjectsData }) {
         <a href={`https://yoomoney.ru/to/410018468894777`} target="_blank"><strong>ЮMoney (Яндекс.Деньги)</strong></a>
         <br>
         </br>
-        Кошельки <strong>WebMoney</strong>: <strong>P535282857177</strong>, <strong>R464312826829</strong> и <strong>Z020618529666</strong>
+        Кошельки <strong>WebMoney</strong>: <strong>P535282857177</strong> и <strong>Z020618529666</strong>
       </section>
     </Layout>
   )
