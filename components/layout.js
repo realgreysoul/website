4c08e0c319d6534587f0c2c1ede91d4239288551
realgreysoul <59358661+realgreysoul@users.noreply.github.com>
2021-01-20 15:12:08 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -14,11 +14,11 @@ export default function Layout({ children, home }) {
       <div className={styles.container}>
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Сайт битмейкера и звукорежиссёра, а также бывшего разработчика инди-игр и бывшего участника многих команд по их разработке" />
-          <meta name="og:title" content={siteTitle} />
-          <meta property="og:type" content="website">
-          <meta name="og:image" content="https://greysoul.ru/images/seo.jpg" />
-          <meta name="robots" content="index, follow">
+          <meta name="description" content="Сайт битмейкера и звукорежиссёра, а также бывшего разработчика инди-игр и бывшего участника многих команд по их разработке" /></meta>
+          <meta name="og:title" content={siteTitle} /></meta>
+          <meta property="og:type" content="website"></meta>
+          <meta name="og:image" content="https://greysoul.ru/images/seo.jpg" /></meta>
+          <meta name="robots" content="index, follow"></meta>
         </Head>
         <header className={styles.header}>
           <Banner home={home} name={name}/>
