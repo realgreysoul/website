27e15b97084b2f77df34e67b20171ed0f497adeb
Greysoul <greysoulgames@gmail.com>
2021-05-22 22:22:10 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
       <footer className={styles.footer}>
         <small class="utils_lightText__12Ckm">
-        <p>© <script type="text/javascript">document.write(new Date().getFullYear())</script> <strong>Greysoul</strong></p>
+        <p>© <span id="Date"></span> <strong>Greysoul</strong></p>
         <p>Основано на <a href={`https://github.com/mwesterby/michaelwesterby.com`} target="_blank"><strong>репозитории</strong></a> с открытым исходным кодом, работает на фреймворке <a href={`https://nextjs.org/`} target="_blank"><strong>Next.js</strong></a> и хостится на платформе <a href={`https://vercel.com/`} target="_blank"><strong>Vercel</strong></a></p>
         </small>
       </footer>
