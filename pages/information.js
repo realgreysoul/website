df36e5b6b404c1a219582f95bfe79e05acb01629
Greysoul <greysoulgames@gmail.com>
2021-05-25 19:23:07 +0700

    Обновление "pw-" на "gs_"

--- pages/information.js
+++ pages/information.js
@@ -15,7 +15,7 @@ export default function Projects({ allProjectsData }) {
         <ul className={utilStyles.list}>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
-        <a href="https://r.greysoul.ru/pw-vk-gsb" target="_blank">Greysoul Beats</a>
+        <a href="https://r.greysoul.ru/gs_vk-gsb" target="_blank">Greysoul Beats</a>
         </div>
         <small class="utils_lightText__12Ckm"><p>Биты на любой вкус и цвет</p></small>
         <small>
@@ -27,7 +27,7 @@ export default function Projects({ allProjectsData }) {
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
-        <a href="https://r.greysoul.ru/pw-vk-x" target="_blank">X1xBuch</a>
+        <a href="https://r.greysoul.ru/gs_vk-x" target="_blank">X1xBuch</a>
         </div>
         <small class="utils_lightText__12Ckm"><p>Дистрибуция музыки и менеджмент артиста</p></small>
         <small>
@@ -39,7 +39,7 @@ export default function Projects({ allProjectsData }) {
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
-        <a href="https://r.greysoul.ru/pw-yt" target="_blank">грейсоль</a>
+        <a href="https://r.greysoul.ru/gs_yt" target="_blank">грейсоль</a>
         </div>
         <small class="utils_lightText__12Ckm"><p>YouTube-канал, на котором я выкладываю всё подряд</p></small>
         <small>
@@ -51,7 +51,7 @@ export default function Projects({ allProjectsData }) {
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
-        <a href="https://r.greysoul.ru/pw-vk-a" target="_blank">Angie</a>
+        <a href="https://r.greysoul.ru/gs_vk-a" target="_blank">Angie</a>
         </div>
         <small class="utils_lightText__12Ckm"><p>Дистрибуция музыки и менеджмент артиста</p></small>
         <small>
@@ -63,7 +63,7 @@ export default function Projects({ allProjectsData }) {
         </li>
         <li class="utils_listItem__2eJpJ">
         <div class="utils_listItemHeading__iji17">
-        <s><a href="https://r.greysoul.ru/pw-gj" target="_blank">Greysoul Games</a></s>
+        <s><a href="https://r.greysoul.ru/gs_gj" target="_blank">Greysoul Games</a></s>
         </div>
         <small class="utils_lightText__12Ckm"><p>Проект закрыт</p></small>
         <small>
@@ -87,41 +87,41 @@ export default function Projects({ allProjectsData }) {
         </li>
         </ul>
         <h3 className={utilStyles.headingXL}>Соц-сети и другие платформы</h3>
-        <a href={`https://r.greysoul.ru/pw-vk`} target="_blank"><strong>ВКонтакте</strong></a>
+        <a href={`https://r.greysoul.ru/gs_vk`} target="_blank"><strong>ВКонтакте</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-ig`} target="_blank"><strong>Instagram</strong></a>
+        <a href={`https://r.greysoul.ru/gs_ig`} target="_blank"><strong>Instagram</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-tg`} target="_blank"><strong>Telegram</strong></a>
+        <a href={`https://r.greysoul.ru/gs_tg`} target="_blank"><strong>Telegram</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-tg-ms`} target="_blank"><strong>Блог</strong></a>
+        <a href={`https://r.greysoul.ru/gs_tg-ms`} target="_blank"><strong>Блог</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-t`} target="_blank"><strong>Twitch</strong></a>
+        <a href={`https://r.greysoul.ru/gs_t`} target="_blank"><strong>Twitch</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-s`} target="_blank"><strong>Steam</strong></a>
+        <a href={`https://r.greysoul.ru/gs_s`} target="_blank"><strong>Steam</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-sc`} target="_blank"><strong>Social Club</strong></a>
+        <a href={`https://r.greysoul.ru/gs_sc`} target="_blank"><strong>Social Club</strong></a>
         <h3 className={utilStyles.headingXL}>Мои плейлисты в ВКонтакте</h3>
-        <a href={`https://r.greysoul.ru/pw-vk-poft`} target="_blank"><strong>Любимые треки</strong></a>
+        <a href={`https://r.greysoul.ru/gs_vk-poft`} target="_blank"><strong>Любимые треки</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-vk-poflft`} target="_blank"><strong>Любимые Lo-Fi треки</strong></a>
+        <a href={`https://r.greysoul.ru/gs_vk-poflft`} target="_blank"><strong>Любимые Lo-Fi треки</strong></a>
         <h3 className={utilStyles.headingXL}>Донат</h3>
-        <a href={`https://r.greysoul.ru/pw-da`} target="_blank"><strong>DonationAlerts</strong></a>
+        <a href={`https://r.greysoul.ru/gs_da`} target="_blank"><strong>DonationAlerts</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-q`} target="_blank"><strong>QIWI Кошелёк</strong></a>
+        <a href={`https://r.greysoul.ru/gs_q`} target="_blank"><strong>QIWI Кошелёк</strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-ym`} target="_blank"><strong>ЮMoney <i>(Яндекс.Деньги)</i></strong></a>
+        <a href={`https://r.greysoul.ru/gs_ym`} target="_blank"><strong>ЮMoney <i>(Яндекс.Деньги)</i></strong></a>
         <br>
         </br>
-        <a href={`https://r.greysoul.ru/pw-vkp`} target="_blank"><strong>VK Pay</strong></a>
+        <a href={`https://r.greysoul.ru/gs_vkp`} target="_blank"><strong>VK Pay</strong></a>
         <br>
         </br>
         Кошельки <strong>WebMoney</strong>:
