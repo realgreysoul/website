e0e2fb309cc2d0ccb8dbb40604507a660144d16b
Greysoul <greysoulgames@gmail.com>
2021-07-11 22:14:34 +0700

    Update information.js

--- pages/information.js
+++ pages/information.js
@@ -108,8 +108,8 @@ export default function Projects({ allProjectsData }) {
         <br>
         </br>
         <a href={`https://socialclub.rockstargames.com/member/RealGreysoul/`} target="_blank"><strong>Social Club</strong></a>
-        <h2 className={utilStyles.headingXL}>Мои плейлисты в Spotify</h2>
-        <a href={`https://open.spotify.com/playlist/4cZ3JghnIH32KSMYYBibM1`} target="_blank"><strong>Любимые Lo-Fi треки</strong></a>
+        <h2 className={utilStyles.headingXL}>Мои плейлисты в ВКонтакте</h2>
+        <a href={`https://vk.com/music/playlist/205955149_91119850_f4156271f2b1885256`} target="_blank"><strong>Любимые Lo-Fi треки</strong></a>
         <h2 className={utilStyles.headingXL}>Донат</h2>
         <a href={`https://www.donationalerts.com/r/realgreysoul`} target="_blank"><strong>DonationAlerts</strong></a>
         <br>
