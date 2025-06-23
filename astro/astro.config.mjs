839644e69dcb100f24c2ef42e254b0dce76b26d0
Greysoul <realgreysoul@outlook.com>
2025-06-24 05:40:52 +0700

    Transfer to Astro framework

---
+++ astro/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://greysoul.ru', // Update with your domain if needed
+  outDir: '../dist',
+  publicDir: './public',
+});
