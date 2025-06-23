d2c983da99413a1fd3872201a5cc955d0ff61071
Greysoul <realgreysoul@outlook.com>
2025-06-24 06:00:33 +0700

    ????

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://greysoul.ru', // Update with your domain if needed
-  outDir: '../dist',
+  outDir: './dist',
   publicDir: './public',
 });
