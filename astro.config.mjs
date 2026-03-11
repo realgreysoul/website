7271fd525619fabd95ec2598ed2496bf4d2a243e
Greysoul <realgreysoul@outlook.com>
2026-03-11 12:56:21 +0700

    Migrating fonts to Astro's Built-in Fonts API, and getting rid of Astro's <Image> in favor of the common <img>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,55 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   site: "https://greysoul.ru",
   outDir: "./dist",
   publicDir: "./public",
   trailingSlash: "always",
+  fonts: [
+    {
+      name: "Inter",
+      cssVariable: "--font-inter",
+      provider: fontProviders.local(),
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/inter-v20-cyrillic_latin-regular.woff2"],
+          },
+          {
+            weight: 400,
+            style: "italic",
+            src: ["./src/assets/fonts/inter-v20-cyrillic_latin-italic.woff2"],
+          },
+          {
+            weight: 700,
+            style: "normal",
+            src: ["./src/assets/fonts/inter-v20-cyrillic_latin-700.woff2"],
+          },
+          {
+            weight: 700,
+            style: "italic",
+            src: [
+              "./src/assets/fonts/inter-v20-cyrillic_latin-700italic.woff2",
+            ],
+          },
+          {
+            weight: 900,
+            style: "normal",
+            src: ["./src/assets/fonts/inter-v20-cyrillic_latin-900.woff2"],
+          },
+          {
+            weight: 900,
+            style: "italic",
+            src: [
+              "./src/assets/fonts/inter-v20-cyrillic_latin-900italic.woff2",
+            ],
+          },
+        ],
+      },
+    },
+  ],
   vite: {
     build: {
       rollupOptions: {
